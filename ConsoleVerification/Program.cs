b78a5e3cc6f42238e67be49bcc988cc20715fbe0
Wasim <wasimrandhawa@gmail.com>
2016-06-03 14:41:22 +0200

    Hello

---
+++ ConsoleVerification/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using ForgetTheMilk.Controllers;
+
+namespace ConsoleVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+          
+
+            TestMayDueDateDoesWrapYear();
+            TestMayDueDateDoesNotWrapYear();
+           
+        
+
+            Console.ReadLine();
+
+        }
+
+        
+        public static void PrintOutcome(bool success, string failureMessage)
+        {
+         if (success)
+            {
+                Console.WriteLine("SUCCESS");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine(failureMessage);
+                
+            }
+
+            Console.WriteLine();
+
+        }
+        private static void TestMayDueDateDoesWrapYear()
+        {
+            var input = "Pickup the groceries may 5 - as of 2015-05-31";
+            Console.WriteLine("Scenario: " + input);
+            var today = new DateTime(2016, 6, 3);
+            var task = new Task(input,today);
+            var dueDateShouldBe = new DateTime(2016, 6, 5);
+            var success = dueDateShouldBe == task.DueDate;
+            var failureMessage = "Due Date: " + task.DueDate + " should be " + dueDateShouldBe;
+            PrintOutcome(success, failureMessage);
+        }
+
+        private static void TestMayDueDateDoesNotWrapYear()
+        {
+            var input = "Pickup the groceries may 5-as of 2015-05-04";
+            Console.WriteLine("Scenario: " + input);
+            var today = new DateTime(2016, 6, 2);
+            var task = new Task(input,today);
+            var dueDateShouldBe = new DateTime(2016, 6, 3);
+            var success = dueDateShouldBe == task.DueDate;
+            var failureMessage= "Due Date: " + task.DueDate + " should be " + dueDateShouldBe;
+            PrintOutcome(success, failureMessage);
+        }
+    }
+}
---
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForgetTheMilk.Controllers
+{
+    public class TaskController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(Tasks);
+        }
+        //public static readonly List<string> Tasks = new List<string>();
+        public static readonly List<Task> Tasks = new List<Task>();
+
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+
+            var taskItem = new Task(task,DateTime.Today);
+            Tasks.Add(taskItem);
+            return RedirectToAction("Index");
+
+        }
+      
+    }
+
+
+    public class Task
+    {
+        public Task(string task,DateTime today)
+        {
+            //var taskItem = new Task { Description = task };
+          Description = task;
+            var dueDatePattern = new Regex(@"june\s(0[1-9]|1[0-9]|2[0-9]|3[0-1]|\d)");
+
+            var hasDueDate = dueDatePattern.IsMatch(task);
+
+            if (hasDueDate)
+            {
+                var dueDate = dueDatePattern.Match(task);
+
+                var day = Convert.ToInt32(dueDate.Groups[1].Value);
+    DueDate = new DateTime(today.Year, 6, day);
+
+         if (DueDate <today)
+              {
+                DueDate = DueDate.Value.AddYears(1);
+       }
+
+            }
+
+        }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
